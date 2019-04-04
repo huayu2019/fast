4eaea307119c6b3757b2436cd5f65d2936a339c8
huayu <hylbx2000@qq.com>
2019-04-04 17:24:54 +0800

    Initial commit

---
+++ src/test/java/cn/com/ifinancesys/fast/JwtTest.java
@@ -0,0 +1,26 @@
+package cn.com.ifinancesys.fast;
+
+import cn.com.ifinancesys.fast.modules.app.JwtUtils;
+import io.jsonwebtoken.Claims;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtTest {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Test
+    public void test() {
+        String token = jwtUtils.generateToken(1);
+
+        System.out.println(token);
+    }
+}
---
+++ src/test/java/cn/com/ifinancesys/fast/service/DynamicDataSourceTestService.java
@@ -0,0 +1,7 @@
+package cn.com.ifinancesys.fast.service;
+
+/**
+ * 测试多数据源
+ */
+public class DynamicDataSourceTestService {
+}
